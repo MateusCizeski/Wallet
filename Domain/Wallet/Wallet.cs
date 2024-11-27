775706534e1e56d1156381d3e4c2df52f91d787a
Mateus Cizeski <mateuscizeski@gmail.com>
2024-11-27 20:19:17 -0300

    mapeando classes iniciais

---
+++ Domain/Wallet/Wallet.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain
+{
+    public class Wallet
+    {
+        public int Id { get; set; }
+        public string Name {  get; set; }
+        public string CpfCnpj { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public decimal Balance { get; set; }
+        public EnumWalletType Type { get; set; }
+        public WalletType WalletType { get; set; }
+    }
+
+    public enum EnumWalletType
+    {
+        Personal,
+        Business
+    }
+}
