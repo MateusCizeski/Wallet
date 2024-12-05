5971f8d52c8dda820baee421c8d8a4998165495d
mateu <mateuscizeski@gmail.com>
2024-12-05 19:44:17 -0300

    Repository da classe walletType

--- Repository/WalletType/IRepWalletType.cs
+++ Repository/WalletType/IRepWalletType.cs
@@ -1,6 +1,13 @@
-﻿namespace Repository.WalletType
+﻿using Domain.WalletType;
+
+namespace Repository.WalletType
 {
     public interface IRepWalletType
     {
+        WalletTypeClass InsertWallet(WalletTypeClass wallet);
+        WalletTypeClass EditWallet(WalletTypeClass wallet);
+        WalletTypeClass GetWalletById(int id);
+        List<WalletTypeClass> ListWallets();
+        void DeleteWallet(int id);
     }
 }
--- Repository/WalletType/RepWalletType.cs
+++ Repository/WalletType/RepWalletType.cs
@@ -11,5 +11,49 @@ namespace Repository.WalletType
         {
             _mongoCollection = mongoCollection;
         }
+
+        public WalletTypeClass InsertWallet(WalletTypeClass wallet)
+        {
+            _mongoCollection.InsertOne(wallet);
+            return wallet;
+        }
+
+        public WalletTypeClass EditWallet(WalletTypeClass wallet)
+        {
+            _mongoCollection.ReplaceOne(filter: p => p.Id == wallet.Id, replacement: wallet);
+
+            return wallet;
+        }
+
+        public WalletTypeClass GetWalletById(int id)
+        {
+            var wallet = _mongoCollection.Find(p => p.Id == id).FirstOrDefault();
+
+            if (wallet == null)
+            {
+                throw new Exception("Carteira não encontrada.");
+            }
+
+            return wallet;
+        }
+
+        public List<WalletTypeClass> ListWallets()
+        {
+            var types = _mongoCollection.Find(_ => true).ToList();
+
+            return types;
+        }
+
+        public void DeleteWallet(int id)
+        {
+            var wallet = _mongoCollection.Find(p => p.Id == id).FirstOrDefault();
+
+            if (wallet == null)
+            {
+                throw new Exception("Carteira não encontrada.");
+            }
+
+            _mongoCollection.DeleteOne(p => p.Id == id);
+        }
     }
 }
